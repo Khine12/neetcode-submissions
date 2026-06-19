74d86d0e248807f7f86715540a5608210e3ccd2b
Khine Zar Hein <khinezarhein1@gmail.com>
2026-06-18 23:42:50 -0700

    Add: surrounded-regions - submission-1

---
+++ Data Structures & Algorithms/surrounded-regions/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        
+        rows = len(board)
+        cols = len(board[0])
+
+        for r in range(rows):
+
+            for c in range(cols):
+
+                if r == 0 or r == rows-1 or c == 0 or c == cols-1 or (board[r-1][c] == "0" and board[r][c-1] == "0" and board[r+1][c] == "0" and board[r][c+1] == "0"):
+
+                    board[r][c] = board[r][c]
+
+                elif 0 < r < rows-1 and 0 < c < cols-1:
+
+                    board[r][c] = "X"
